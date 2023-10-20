8a051e2b41ac8774399fdb5fa4aadf6c9be1e001
TaiMenegatti-dev <tainam.vieira@gmail.com>
2023-10-20 10:56:23 -0300

    logout

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include 'conexao.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Suponhamos que você já tenha recebido o nome de usuário e senha do usuário
@@ -6,18 +7,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['senha_login'];
 
     // Consulta o banco de dados para verificar as credenciais
-    $query = "SELECT funcionario_email, funcionario_senha FROM funcionario WHERE funcionario_email = '$email'";
+    $query = "SELECT * FROM funcionario WHERE funcionario_email = '$email'";
     $result = $mysqli->query($query);
 
     if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
         $db_password = $row['funcionario_senha'];
+        $_SESSION["usuario_id"] = $row['funcionario_id'];
 
         // Verifica se a senha fornecida corresponde à senha no banco de dados
         if (password_verify($password, $db_password)) {
             // Senha válida, você pode continuar com a autenticação no site
             // Use cURL ou outras bibliotecas para acessar o site
-            echo "Login bem-sucedido!";
+
             header('location:especiais.php');
         } else {
             echo "Senha incorreta";
