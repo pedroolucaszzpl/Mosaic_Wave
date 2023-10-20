419f1c6627eecc6084d7b3c5251626f52ae13d71
em1llyVilela <emillyyvilela@gmail.com>
2023-10-20 13:35:35 -0300

    novos

--- login.php
+++ login.php
@@ -4,20 +4,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Suponhamos que você já tenha recebido o nome de usuário e senha do usuário
     $email = $_POST['nome_login']; // Suponha que esses valores sejam recebidos de um formulário HTML
     $password = $_POST['senha_login'];
-    
+
     // Consulta o banco de dados para verificar as credenciais
     $query = "SELECT * FROM funcionario WHERE funcionario_email = '$email'";
     $result = $mysqli->query($query);
-    
+
     if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
         $db_password = $row['funcionario_senha'];
-<<<<<<< HEAD
-        
-=======
+
         $_SESSION["usuario_id"] = $row['funcionario_id'];
 
->>>>>>> c64d4b2012f6abd0cdb920f6725c5c8182bb998a
         // Verifica se a senha fornecida corresponde à senha no banco de dados
         if (password_verify($password, $db_password)) {
             // Senha válida, você pode continuar com a autenticação no site
