93ba66e030a1d8f28ba4bf0747a251cd205a3d7e
TaiMenegatti-dev <tainam.vieira@gmail.com>
2023-11-13 14:28:50 -0300

    b

--- mostruario.php
+++ mostruario.php
@@ -1,168 +1,196 @@
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/mostruario.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-    <!--Google Link Icon-->
-    <title>IntenseStreet Calçados</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/mostruario.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+        <!--Google Link Icon-->
+        <title>IntenseStreet Calçados</title>
+    </head>
 
-<body>
-    <header>
-        <nav class="navbar">
-            <div class="contentHead">
-                <div class="navbar-menu">
-                    <ul class="navbar-items">
-                        <li class="navbar-items-menu">
-                            <a class="nav-link" href="main.php" onclick="">Páginal Inicial</a>
-                        </li>
-                        <li><select name="departamentos" id="departamentos" onchange="redirectToPage(this)">
-                                <option value="" disabled selected hidden>Departamentos</option>
-                                <option value="calcados">Calçados</option>
-                                <option value="acessorios">Acessórios</option>
-                                <option value="vestuario">Vestuário</option>
-                                <option value="especial">Edições Especiais</option>
+    <body>
+        <header>
+            <nav class="navbar">
+                <div class="contentHead">
+                    <div class="navbar-menu">
+                        <ul class="navbar-items">
+                            <li class="navbar-items-menu">
+                                <a class="nav-link" href="main.php" onclick="">Páginal Inicial</a>
+                            </li>
+                            <li><select name="departamentos" id="departamentos" onchange="redirectToPage(this)">
+                                    <option value="" disabled selected hidden>Departamentos</option>
+                                    <option value="calcados">Calçados</option>
+                                    <option value="acessorios">Acessórios</option>
+                                    <option value="vestuario">Vestuário</option>
+                                    <option value="especial">Edições Especiais</option>
 
-                            </select>
-                        </li>
-                    </ul>
+                                </select>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="menu_vertical">
+                    <span id="icon" class="material-symbols-outlined" onclick="clickMenu()">
+                        menu
+                    </span>
+                    <menu id="mvertical">
+                        <ul>
+                            <li><a href="main.php">Página Inicial</a></li>
+                            <li><a href="main.php">Calçados</a></li>
+                            <li><a href="main.php">Acessórios</a></li>
+                            <li><a href="main.php">Vestuário</a></li>
+                            <li><a href="especiais.php">Edições Especiais</a></li>
+                        </ul>
+                    </menu>
+                </div>
+                <div class="search">
+                    <form class="formss" action="pesquisa.php" method="post">
+                        <input type="image" id="lupa" src="img/lupa.png" alt="lupa">
+                        <input name='termo' type="text" placeholder=" O que você precisa?" class="ask">
+                    </form>
+                </div>
+                <div class="buy">
+                    <a href="carrinho.php"><input type="image" id="carrinho" src="img/carrinho.png" alt="">
+                </div>
+                <?php
+                if (!isset($_SESSION["usuario_id"])) {
+                    echo    "<div class='logarConta'>";
+                    echo    "<a href='logar.php'>Logar</a>";
+                    echo    "</div>";
+                }
+                if (isset($_SESSION["usuario_id"])) {
+                    echo    "<div class='logout'>";
+                    echo    "<a href='logout.php'>Sair</a>";
+                    echo    "</div>";
+                }
+                ?>
+            
+                <a class="navbar-logo" href="main.php">
+                    <img src="img/logo1.png" alt="Logo IntenseStreet" description="Logo IntenseStreet" id="logo1">
+                </a>
+            </nav>
+        </header>
+        <main>
+            <div class="barra">
+                <div id="linhas">
+                    <div class="line"></div>
+                    <p class="pag">DESCRIÇÃO</p>
+                    <div class="line"></div>
                 </div>
             </div>
-            <div id="menu_vertical">
-                <span id="icon" class="material-symbols-outlined" onclick="clickMenu()">
-                    menu
-                </span>
-                <menu id="mvertical">
-                    <ul>
-                        <li><a href="main.php">Página Inicial</a></li>
-                        <li><a href="main.php">Calçados</a></li>
-                        <li><a href="main.php">Acessórios</a></li>
-                        <li><a href="main.php">Vestuário</a></li>
-                        <li><a href="especiais.php">Edições Especiais</a></li>
-                    </ul>
-                </menu>
-            </div>
-            <div class="search">
-                <form class="formss" action="pesquisa.php" method="post">
-                    <input type="image" id="lupa" src="img/lupa.png" alt="lupa">
-                    <input name='termo' type="text" placeholder=" O que você precisa?" class="ask">
-                </form>
-            </div>
-            <div class="buy">
-                <a href="carrinho.php"><input type="image" id="carrinho" src="img/carrinho.png" alt="">
-            </div>
-            <?php
-            if (!isset($_SESSION["usuario_id"])) {
-                echo    "<div class='logarConta'>";
-                echo    "<a href='logar.php'>Logar</a>";
-                echo    "</div>";
-             }
-            if (isset($_SESSION["usuario_id"])) {
-                echo    "<div class='logout'>";
-                echo    "<a href='logout.php'>Sair</a>";
-                echo    "</div>";
-            }
-            ?>
-            <a class="navbar-logo" href="main.php">
-                <img src="img/logo1.png" alt="Logo IntenseStreet" description="Logo IntenseStreet" id="logo1">
-            </a>
-        </nav>
-    </header>
-    <main>
-        <div class="barra">
-            <div id="linhas">
-                <div class="line"></div>
-                <p class="pag">DESCRIÇÃO</p>
-                <div class="line"></div>
+                <?php
+                session_start();
+                include 'conexao.php';
+                $tenis_id = $_GET['id'];
+                $sql = "SELECT * FROM tenis WHERE tenis_id = '$tenis_id'";
+                $resultado = $mysqli->query($sql);
+
+                if ($resultado) {
+                    while ($row = $resultado->fetch_assoc()) {
+                        echo "<div class='desc_produto'>";
+                        echo "<div class='principal'>";
+                        echo "<img class='img2' src='" . $row["tenis_img"] . "'>";
+                        echo "</div>";
+                        echo "<div class='descricao'>";
+                        echo "<div class='nome'>";
+                        echo "<h3>" . $row["tenis_modelo"] . "</h3>";
+                        echo "</div>";
+                        echo "<div class='cor'>";
+                        echo "<label for='cor'>Cor:</label>";
+                        echo "<p class='desc'>" . $row["tenis_cor"] . "</p>";
+                        echo "</div>";
+                        echo "<div class='cor'>";
+                        echo "<label for='tamanho'>Tamanho:</label>";
+                        echo "<p class='desc'>" . $row["tenis_tamanho"] . "</p>";
+                        echo "</div>";
+                        echo "<div class='cor'>";
+                        echo "<p class='price'>R$" . $row["tenis_preco"] . "</p>";
+                        echo "</div>";
+                        echo "<div class='pagamento'>";
+                        echo "<label for='pagamento'>Forma de Pagamento:</label>";
+                        echo "<select id='pagamento'>";
+                        echo "<option value='credito'>Cartão de Crédito</option>";
+                        echo "<option value='boleto'>Boleto</option>";
+                        echo "<option value='Pix'>Pix</option>";
+                        echo "</select>";
+                        echo "</div>";
+                        echo "<div class='botoes-mostruario'>";
+                        echo "<button id='botao-add' onclick='addToCart(". $row["tenis_modelo"] .")'>COMPRAR AGORA</button>";
+                        echo "<button id='botao-add' onclick='showCarrinhoModal(". $row["tenis_modelo"] .")'>ADICIONAR AO CARRINHO</button>";                     
+                        echo "</div>";
+                        echo "</div>";
+                        echo "</div>";
+                    }
+                }
+                //    }
+                ?>
+        </main>
+        <!-- Início do Rodapé -->
+        <div class="footer-clean">
+            <footer>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-sm-4 col-md-3 item">
+                            <h3>Serviços</h3>
+                            <ul>
+                                <li><a href="#">Compras</a></li>
+                                <li><a href="#">Entrega</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-4 col-md-3 item">
+                            <h3>Sobre</h3>
+                            <ul>
+                                <li><a href="#">Companhia</a></li>
+                                <li><a href="#">Quem Somos nós?</a></li>
+                                <li><a href="#">Politica</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-4 col-md-3 item">
+                            <h3></h3>
+                            <ul>
+                                <li><a href="#"><i class="icon ion-social-facebook"></i></a></li>
+                                <li><a href="#"><i class="icon ion-social-twitter"></i></a></li>
+                                <li><a href="#"><i class="icon ion-social-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <!-- Adicione esta parte ao seu HTML, geralmente no final do corpo -->
+        <div class="modal" id="carrinhoModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Item Adicionado ao Carrinho</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Aqui você pode adicionar o conteúdo do seu modal -->
+                        <p>Produto adicionado ao carrinho com sucesso!</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                    </div>
+                </div>
             </div>
         </div>
-            <?php
-            session_start();
-            include 'conexao.php';
-            $tenis_id = $_GET['id'];
-            $sql = "SELECT * FROM tenis WHERE tenis_id = '$tenis_id'";
-            $resultado = $mysqli->query($sql);
 
-            if ($resultado) {
-                //          if ($resultado->num_rows() > 0) {
-                while ($row = $resultado->fetch_assoc()) {
-                    echo "<div class='desc_produto'>";
-                    echo "<div class='principal'>";
-                    echo "<img class='img2' src='" . $row["tenis_img"] . "'>";
-                    echo "</div>";
-                    echo "<div class='descricao'>";
-                    echo "<div class='nome'>";
-                    echo "<h3>" . $row["tenis_modelo"] . "</h3>";
-                    echo "</div>";
-                    echo "<div class='cor'>";
-                    echo "<label for='cor'>Cor:</label>";
-                    echo "<p class='desc'>" . $row["tenis_cor"] . "</p>";
-                    echo "</div>";
-                    echo "<div class='cor'>";
-                    echo "<label for='tamanho'>Tamanho:</label>";
-                    echo "<p class='desc'>" . $row["tenis_tamanho"] . "</p>";
-                    echo "</div>";
-                    echo "<div class='cor'>";
-                    echo "<p class='price'>R$" . $row["tenis_preco"] . "</p>";
-                    echo "</div>";
-                    echo "<div class='pagamento'>";
-                    echo "<label for='pagamento'>Forma de Pagamento:</label>";
-                    echo "<select id='pagamento'>";
-                    echo "<option value='credito'>Cartão de Crédito</option>";
-                    echo "<option value='boleto'>Boleto</option>";
-                    echo "<option value='Pix'>Pix</option>";
-                    echo "</select>";
-                    echo "</div>";
-                    echo "<div class='botoes-mostruario'>";
-                    echo "<button id='botao-add' onclick='addToCart(". $row["tenis_modelo"] .", ". $row["tenis_preco"] .")'>COMPRAR AGORA</button>";
-                    echo "<button id='botao-add' onclick='addToCart(". $row["tenis_modelo"] .", ". $row["tenis_preco"] .")'>ADICIONAR AO CARRINHO </button>";
-                    echo "</div>";
-                    echo "</div>";
-                    echo "</div>";
-                }
+        <script src="js/homep.js" async></script>
+        <script src="js/main.js" defer></script>
+        <!-- Adicione esta parte ao seu HTML, geralmente antes do fechamento do corpo -->
+        <script>
+            function showCarrinhoModal() {
+                // Aqui você pode adicionar lógica adicional antes de exibir o modal, se necessário
+                $('#carrinhoModal').modal('show');
             }
-            //    }
-            ?>
-    </main>
-    <!-- Início do Rodapé -->
-    <div class="footer-clean">
-        <footer>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-sm-4 col-md-3 item">
-                        <h3>Serviços</h3>
-                        <ul>
-                            <li><a href="#">Compras</a></li>
-                            <li><a href="#">Entrega</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-4 col-md-3 item">
-                        <h3>Sobre</h3>
-                        <ul>
-                            <li><a href="#">Companhia</a></li>
-                            <li><a href="#">Quem Somos nós?</a></li>
-                            <li><a href="#">Politica</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-4 col-md-3 item">
-                        <h3></h3>
-                        <ul>
-                            <li><a href="#"><i class="icon ion-social-facebook"></i></a></li>
-                            <li><a href="#"><i class="icon ion-social-twitter"></i></a></li>
-                            <li><a href="#"><i class="icon ion-social-instagram"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    </div>
-    <script src="js/homep.js" async></script>
-    <script src="js/main.js" defer></script>
-</body>
+        </script>
+    </body>
 
-</html>
+    </html>
